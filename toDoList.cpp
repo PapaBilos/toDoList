716a13ab9d9d7738b148a6456dee7f030270ce61
Bartosz Kowaliczek <bartoszkowaliczek20@gmail.com>
2023-03-01 13:11:40 +0100

    Basic editing option added. Need to recode fragments where user input lines (strings) with white spaces

--- toDoList.cpp
+++ toDoList.cpp
@@ -12,11 +12,11 @@ Quick toDoList program with four options:
 #include<fstream>
 #include<string> //getline()
 
-std::vector<int> options {1,2,3,4,5};
-std::vector<int>::iterator it;
+std::vector<unsigned int> options {1,2,3,4,5};
+std::vector<unsigned int>::iterator it;
 
-int taskID;
-
+unsigned int taskID;
+// const std::string taskFileName = "tasks.txt"; //solve problem !
 const std::string mainMenuOptions = "Welcome to toDoList v0.1\n"
 "1. Add task\n"
 "2. Read task(s)\n"
@@ -25,20 +25,32 @@ const std::string mainMenuOptions = "Welcome to toDoList v0.1\n"
 "5. Quit program\n"
 "Select option: ";
 
+bool actionDone(){
+    std::cout << "Done? If so, type 'y' or 'Y':" << std::endl;
+    char done;
+    std::cin >> done;
+    if(done != 'y' && done != 'Y'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 void addTask(){
     char repeat;
     do
     {
         std::string taskName;
         system("cls");
-        std::cout << "Adding task" << std::endl << std::endl;
+        std::cout << "Adding task\n\n";
         std::cout << " Enter new task: ";
         std::cin >> taskName;
 
         std::fstream tasksFile;
         tasksFile.open("tasks.txt",std::fstream::out | std::fstream::app);
         if(tasksFile.is_open()){
-            tasksFile << taskName << "\n" ;
+            tasksFile << taskName << "\n";
             tasksFile.close();
         }
         else{
@@ -62,7 +74,7 @@ void addTask(){
     
 }
 
-void readTask(){
+void showTasks(){
     system("cls");
     std::cout << "Reading task(s)" << std::endl;
 
@@ -76,7 +88,6 @@ void readTask(){
         {
             std::getline(idFile,idFileLine);
             std::getline(tasksFile,tasksFileLine);
-
             std::cout << idFileLine << "\t" << tasksFileLine << std::endl;
         }
         idFile.close();
@@ -85,28 +96,66 @@ void readTask(){
     else{
         std::cout << "ERROR::readTask::FILE(s)_NOT_OPEN";
     }
-
-    std::cout << "Done? If so, tyle 'y' or 'Y':" << std::endl;
-    char done;
-    do{
-        std::cin >> done;
-    }while(done != 'y' && done != 'Y');
-    system("cls");
 }
 
 void editTask(){
+    int taskToDel;
+    int iter = 0;
+    std::string newTask;
+
     system("cls");
     std::cout << "Editing task" << std::endl;
+    std::cout << "There is list of tasks. Choose with one you want to edit: \n\n";
+    showTasks();
+
+    std::cout << "Type num. of task to edit.: ";
+    std::cin >> taskToDel;
+
+    if(taskToDel > 0 || taskToDel <= taskID){
+        std::fstream taskFile, taskFileTemp;
+        std::string line;
+        taskFile.open("tasks.txt",std::fstream::in);
+        taskFileTemp.open("tasksTemp.txt",std::fstream::out);
+        if(taskFile.good() && taskFileTemp.good()){
+            while (taskFile.peek() != EOF)
+            {   
+                iter++;
+                std::getline(taskFile,line);
+
+                if(iter != taskToDel){
+                    taskFileTemp << line << "\n";
+                }
+                else{
+                    std::cout << "Task you picked: \n" << taskToDel << "\t" << line << std::endl;
+                    std::cout << "Enter new name of task: ";
+                    std::getline(std::cin, newTask);
+                    taskFileTemp << newTask << "\n";
+                }
+            }
+        }
+        std::cout<< "INFO::Task edited succesfully";
+
+        taskFile.close();
+        taskFileTemp.close();
+
+        std::remove("tasks.txt");
+        std::rename("tasksTemp.txt","tasks.txt");
+        std::remove("tasksTemp.txt");
+    }
+    else{
+        std::cout << "INFO::Task num. out of bound!\n";
+    }
+    system("cls");
 }
 
 void removeTask(){
     system("cls");
     std::cout << "Removing task" << std::endl;
-    
+    std::cout << "There is list of tasks. Choose with one you want to delete: \n\n";
+    showTasks();
 }
 
 void idCheck(){
-    //HOW TO GET LAST LINE FROM id.txt ???
     std::fstream idFile;
     idFile.open("id.txt",std::fstream::in);
 
@@ -129,7 +178,9 @@ void idCheck(){
 int main(){
     system("cls");
     idCheck();
-
+    // char check[100];
+    // std::cin.getline(check,100);
+    // std::cout << check;
     do{
         //Var to choose option
         int choice;
@@ -140,13 +191,17 @@ int main(){
         it = std::find(options.begin(), options.end(), choice);
 
         if(it != options.end()){
-            switch (choice)
-            {
+            switch (choice){
             case 1:
+                //done
                 addTask();
+                system("cls");
                 break;
             case 2:
-                readTask();
+                //done
+                showTasks();
+                while (actionDone());
+                system("cls");
                 break;
             case 3:
                 editTask();
@@ -155,6 +210,7 @@ int main(){
                 removeTask();
                 break;
             case 5:
+                //done
                 std::cout << "Thanks for using this program!!!" << std::endl;
                 return 0;
                 break;
@@ -167,4 +223,3 @@ int main(){
     while(true);
     return 0;
 }
-
